0d50429c2a970c43884f146359e4d9d63784a5c7
Retro <rdgc_95@hotmail.es>
2022-08-13 19:28:54 -0600

    added mic and speaker SPEAKER NOT TESTED YET

--- setup.sh
+++ setup.sh
@@ -3,6 +3,17 @@ echo "Starting setup..."
 echo " "
 echo "Creating files directory"
 mkdir ./files
+read -p "apt-get update & upgrade? Y/n... " -n 1 -r
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+    echo " "
+    echo "Installing emacs..."
+    sudo apt-get -y update
+    sudo apt-get -y upgrade
+    echo " "
+    echo "Update and Upgrade done "
+fi
+echo " "
 read -p "Install Emacs? Y/n... " -n 1 -r
 if [[ ! $REPLY =~ ^[Nn]$ ]]
 then
@@ -41,10 +52,38 @@ then
     sudo pip3 install rpi_ws281x adafruit-circuitpython-neopixel
     sudo cp ./code/snd-blacklist.conf /etc/modprobe.d/snd-blacklist.conf
     echo " "
-    echo "Note snd_bcm2835 has been blacklisted!"
+    echo "Note snd_bcm2835 has been blacklisted! Tip from: https://github.com/jgarff/rpi_ws281x"
     echo " "
     echo "Neopixels driver installation done!"
-    
+fi
+echo " "
+read -p "Install INA219 Battery Sensor Drivers? Y/n... " -n 1 -r
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+    echo " "
+    echo "Installing necessary drivers..."
+    sudo pip3 install adafruit-circuitpython-ina219
+    echo "INA219 driver installation done!"
+fi
+echo " "
+read -p "Install MAX98357 I2S Speaker AMP? Y/n... " -n 1 -r
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+    echo " "
+    echo "Installing necessary drivers..."
+    curl -sS https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2samp.sh | bash
+    echo "INA219 driver installation done!"
+fi
+echo " "
+read -p "Install SPH0645 I2S MIC? Y/n... " -n 1 -r
+if [[ ! $REPLY =~ ^[Nn]$ ]]
+then
+    echo " "
+    echo "Installing necessary drivers..."
+    sudo pip3 install --upgrade adafruit-python-shell
+    wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/i2smic.py ./code/mic
+    sudo python3 ./code/mic/i2smic.py
+    echo "SPH0645 driver installation done!"
 fi
 echo " "
 read -p "Create Startup Script? Y/n... " -n 1 -r
